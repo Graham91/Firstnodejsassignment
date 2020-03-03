dc9b759821cc0a70472ade3a01f70233bffa30a4
grambo <gramham@live.com>
2020-03-03 14:45:29 -0700

    finished all options for Use document option

--- liri.js
+++ liri.js
@@ -77,12 +77,76 @@ else if(answers.searchtype === 'Use Document'){
     },]).then(answers => { 
       
       var newFilePath = answers.filepath;
-      console.log(newFilePath);
+      
      const fs = require('fs');
       fs.readFile(newFilePath, (err, data) => { 
         if (err) throw err; 
-      
-        console.log(data.toString()); 
+        var datafromtextfile = [];
+        
+        var newstring = data.toString();
+        datafromtextfile = newstring.split(",");
+if (datafromtextfile[0] === 'spotify-this-song'){
+  spotify.search({ type: 'track', query: datafromtextfile[1] }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    
+  console.log("Song Name: " + data.tracks.items[0].name);
+  console.log("Artist: " + data.tracks.items[0].artists[0].name);
+  console.log("Album: " + data.tracks.items[0].album.name);
+  console.log("check out Song at this URL: " +
+   data.tracks.items[0].external_urls.spotify);
+  });
+}
+else if (datafromtextfile[0] === 'movie this'){
+  axios({
+    method: 'get',
+    url:"http://www.omdbapi.com/?apikey=daba8cc8&"+"s=" + datafromtextfile[1],
+  }).then(function (response) {
+    console.log("Movie Title: "+response.data.Search[0].Title);
+    console.log("Year: "+response.data.Search[0].Year);
+
+  //   * Title of the movie.
+  //  * Year the movie came out.
+  //  * IMDB Rating of the movie.
+  //  * Rotten Tomatoes Rating of the movie.
+  //  * Country where the movie was produced.
+  //  * Language of the movie.
+  //  * Plot of the movie.
+  //  * Actors in the movie.
+
+});
+}
+else{
+  axios({
+    method: 'get',
+    url: "https://rest.bandsintown.com/artists/" + datafromtextfile[1] + "/events?app_id=codingbootcamp",
+  }).then(function (response) {
+    var moment = require('moment');
+     var time1 = moment(response.data[0].datetime).format("MM/DD/YYYY");
+     var time2 = moment(response.data[1].datetime).format("MM/DD/YYYY");
+     var time3 = moment(response.data[2].datetime).format("MM/DD/YYYY");
+
+
+console.log("Date: "+time1);
+console.log("Venue: " + response.data[0].venue.name)
+console.log("Venue Location: " +response.data[0].venue.city + ", "+  response.data[0].venue.country)
+console.log("----------------------");
+console.log("Date: "+time2);
+console.log("Venue: " + response.data[1].venue.name)
+console.log("Venue Location: " +response.data[1].venue.city + ", "+  response.data[1].venue.country)
+console.log("----------------------");
+console.log("Date: "+time3);
+console.log("Venue: " + response.data[2].venue.name)
+console.log("Venue Location: " +response.data[2].venue.city + ", "+  response.data[2].venue.country)
+
+// * Name of the venue
+
+// * Venue location
+
+// * Date of the Event (use moment to format this as "MM/DD/YYYY")
+});
+}
     }) 
     })
 }
